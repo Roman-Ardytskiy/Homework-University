9d9d759dfe8a3d1e5b451e3fbba986a6c78e7056
Roman-Ardytskiy <romanardytskiy@gmail.ru>
2018-11-08 21:46:27 +0300

    второй пункт
    добавил: счетчики, поля ввода, чекбокс, очень много обработок

--- js.js
+++ js.js
@@ -1,40 +1,114 @@
 function process() {
+  
+
+
+  count();
+  checking();
+  
+
+
+
+ 
+
+   
+}
+
+
+function checking() {
 
   var time = document.Table.listTimeName.value;
-  var persons = document.Table.listPersonsName.value;
-  document.Table.field.value = " Стол на " +persons + " в " + time ;
-
-}
-
-var t=[0,0,0];
-
-function countTime() {
-
-  
-  disabledButton(t)
-  if( document.Table.listTimeName.value  =="17:00 - 19:00") {
-    t[0]++;
-  } else if( document.Table.listTimeName.value == "19:00 - 21:00") {
-    t[1]++;
-  } else { 
-    t[2]++;
-        }
-
-        console.log( t);
-}
-
-
-
-function disabledButton(count) {
-
-for ( var i  = 0; i < count.length; i++) {
-  if( count > 4) {
-    document.getElementById('add-button').disabled= true;
-  } else { 
-    document.getElementById('add-button').disabled= false; 
+  var persons = +document.getElementById('add-input').value;
+  var checks = document.getElementsByClassName('checkbox');
+  if( checks[0].checked) { 
+    document.Table.field.value = "В " + time + " Столик на " + persons +" быстрый ужин";
+  } else {
+    document.Table.field.value = "В " + time + " Столик на " + persons;
   }
 }
 
+function count() {
+
+  var counter1_19 = 0;
+  var counter2_21 = 0;
+
+  var checks = document.getElementsByClassName('checkbox');
+  var persons = +document.getElementById('add-input').value;
+  var time = document.Table.listTimeName.value;
+
+  if( time =="19:00 - 21:00" ) {
+    if( persons <= 4) {
+      if ( checks[0].checked ){
+        counter1_19 = counterOne_19();
+      } else {
+        counter1_19 = counterOne_19();
+        counter1_19 = counterOne_19();
+      }
+    } else {
+     if ( checks[0].checked ){
+        counter1_19 = counterOne_19();
+        counter1_19 = counterOne_19();
+      } else {
+        counter1_19 = counterOne_19();
+        counter1_19 = counterOne_19();
+        counter1_19 = counterOne_19();
+        counter1_19 = counterOne_19();
+      } 
+    }
+   
+
+    console.log( "counter1 = " +counter1_19 );
+    (  counter1_19 > 8  ) ? alert( "Свободных столиков нет") :
+    alert(" Заказ может быть оформлен");
+  } else { 
+    if( persons <= 4) {
+      if ( checks[0].checked ){
+        counter2_21 = counterOne_21();
+      } else {
+        counter2_21 = counterOne_21();
+        counter2_21 = counterOne_21();
+      }
+    } else {
+     if ( checks[0].checked ){
+        counter2_21 = counterOne_21();
+        counter2_21 = counterOne_21();
+      } else {
+        counter2_21 = counterOne_21();
+        counter2_21 = counterOne_21();
+        counter2_21 = counterOne_21();
+        counter2_21 = counterOne_21();
+      } 
+    }
+   
+
+    console.log( "counter2 = " +counter2_21 );
+    (  counter2_21 > 8  ) ? alert( "Свободных столиков нет") :
+    alert(" Заказ может быть оформлен");
+
+  }
+
+ 
   
 
 }
+
+
+
+
+function makeCounter1() {
+  var currentCount = 0;
+  return function() {
+      return ++currentCount;
+  }
+};
+
+function makeCounter2() {
+  var currentCount = 0;
+  return function() {
+      return ++currentCount,++currentCount;
+  }
+};
+
+
+
+var counterOne_19 = makeCounter1();
+var counterOne_21 = makeCounter1();
